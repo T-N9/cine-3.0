57623febce84a836fb2cd4b255950da7679a4dd2
T-N9 <thethoriumtension@gmail.com>
2023-03-09 08:46:09 +0630

    [Project] ini

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1>CINE 3.0 has been initialized.</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
