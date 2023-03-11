8f4edcefd60bb14dd2a982141891cc808569bd01
T-N9 <thethoriumtension@gmail.com>
2023-03-11 07:58:39 +0630

    [ServerSide] test

--- pages/movies/[movieId].js
+++ pages/movies/[movieId].js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const MovieId = () => {
+const MovieId = ({message}) => {
   return (
     <div>
       <h1>Hello</h1>
+      <p>{message}</p>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: { message: `Next.js is awesome` }, // will be passed to the page component as props
+  }
 }
 
 export default MovieId;
